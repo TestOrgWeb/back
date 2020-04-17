c5ed5136e40ec671a72e28cb9d516a350b4afc6c
Sayali Gaikawad <gaiksaya@amazon.com>
2020-04-17 13:02:03 -0700

    Merge Conflict here

--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
 print("This is EOD!")
+<<<<<<< HEAD
 print("This is branch-1.0")
 print("This is branch-1.1")
+=======
+print("This is from second and third feature branch")
+print("This is branch-1.1")
+print("This is branch-1.2")
+>>>>>>> 8c2d609... Third added
