09e1c77dcbac45c082e4ea28f35930857c4f1a80
Sayali Gaikawad <61760125+gaiksaya@users.noreply.github.com>
2020-04-20 15:51:14 -0700

    Resolving conflicts

--- hello.py
+++ hello.py
@@ -1,10 +1,4 @@
 print("This is EOD!")
-<<<<<<< HEAD
-print("This is from second and third feature branch")
-print("This is branch-1.1")
-print("This is branch-1.2")
-=======
 print("This is an attempt from feature branch")
 print("This is branch-1.1")
 print("This is branch-1.2")
->>>>>>> 14046fa... attempt
