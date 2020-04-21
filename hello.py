77e8707f69cdee94cc707a205c08b4c88d625642
Sayali Gaikawad <61760125+gaiksaya@users.noreply.github.com>
2020-04-21 13:02:14 -0700

    Resolving conflicts

--- hello.py
+++ hello.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-print("This is EOD!")
-print("This is branch-1.0")
-=======
 #Let's try backporting
 print("This is an attempt from feature branch")
 print("This is branch-1.1")
 print("This is branch-1.2")
->>>>>>> f878b23... New feature test
