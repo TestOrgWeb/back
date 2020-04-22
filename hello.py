90062de8192440cc233e26ce76a9d56859aba01b
Sayali Gaikawad <61760125+gaiksaya@users.noreply.github.com>
2020-04-22 15:27:13 -0700

    Resolving commits

--- hello.py
+++ hello.py
@@ -2,8 +2,5 @@
 print("This is an attempt from feature branch")
 print("This is branch-1.1")
 print("This is branch-1.2")
-<<<<<<< HEAD
-=======
 print ("This is 2nd commit")
 print ("This is 3rd commit")
->>>>>>> ba3766d... Third commit
