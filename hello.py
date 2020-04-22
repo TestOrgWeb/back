a6a7483a0a28f54631daba406dd8214be60c6076
Sayali Gaikawad <gaiksaya@amazon.com>
2020-04-22 15:23:59 -0700

    Merge Conflicts here!

--- hello.py
+++ hello.py
@@ -2,3 +2,8 @@
 print("This is an attempt from feature branch")
 print("This is branch-1.1")
 print("This is branch-1.2")
+<<<<<<< HEAD
+=======
+print ("This is 2nd commit")
+print ("This is 3rd commit")
+>>>>>>> ba3766d... Third commit
